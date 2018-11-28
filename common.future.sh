35dcca09a29fc82746d65eee617be1d045cdda21
Brandon Riden <brandon.riden@onyxpoint.com>
2018-11-28 13:02:08 -0500

    fix common.sh

--- common.future.sh
+++ common.future.sh
@@ -11,7 +11,7 @@ fi
 # Set global dir variables
 export source_dir="$(pwd)"
 export VERSION=${VERSION:=nightly-$(timedatectl | head -n 1 | awk '{print $4}')}
-export BUILDDIR="${source_dir}/rubygem-simp-metadata/build"
+export BUILDDIR="${source_dir}/build"
 export BINARIESDIR="${source_dir}/binaries"
 export EL_VERSION=${EL_VERSION:=el7}
 export PLATFORM=${PLATFORM}
@@ -22,8 +22,6 @@ echo "${SSH_KEY}" > ./ssh_key
 chmod 700 ./ssh_key
 export SIMP_METADATA_SSHKEY="$(pwd)/ssh_key"
 export GIT_SSH="$(pwd)/git_ssh_wrapper.sh"
-
-git clone https://github.com/brandonrdn/rubygem-simp-metadata
 
 # Purge global dirs
 rm -rf "${BUILDDIR}"
@@ -36,7 +34,9 @@ if [ ! -d "${BINARIESDIR}" ];then
 mkdir -p "${BINARIESDIR}"
 fi
 
-git clone https://github.com/brandonrdn/rubygem-simp-metadata
-cd rubygem-simp-metadata
-bundle install
-export RUBYGEM_DIR="$(pwd)"
+# Install simp-metadata gem
+export METADATA_VERSION=${METADATA_VERSION:=0.4.3}
+if [ ! -f simp-metadata-${METADATA_VERSION}.gem ];then
+wget https://download.simp-project.com/simp/assets/rubygem-simp-metadata/simp-metadata-${METADATA_VERSION}.gem
+fi
+gem install --local --pre simp-metadata-${METADATA_VERSION}.gem
